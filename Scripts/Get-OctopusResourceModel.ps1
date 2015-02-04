3f91c4851119754e223c5916d322396257f6f77a
Dalmiro <dalmiro.granias@gmail.com>
2015-02-04 00:09:22 -0300

    Added help to Get-OctopusResourceModel

---
+++ Scripts/Get-OctopusResourceModel.ps1
@@ -0,0 +1,40 @@
+﻿<#
+.Synopsis
+   Returns an empty object from the Octopus Model that can later on be piped down to New-OctopusResource
+.DESCRIPTION
+   Returns an empty object from the Octopus Model that can later on be piped down to New-OctopusResource
+.EXAMPLE
+   $EnvironmentObj = Get-OctopusResourceModel -Resource "Environment"
+.EXAMPLE
+   $ProjectObj = Get-OctopusResourceModel -Resource "Project"
+#>
+function Get-OctopusResourceModel
+{
+    [CmdletBinding()]
+    Param
+    (
+        # Resource object model
+        [Parameter(Mandatory=$true,                  
+                   Position=0)]
+        [ValidateSet("Environment","Project","ProjectGroup")] 
+        $Resource
+    )
+
+    Begin
+    {
+        $c = New-OctopusConnection
+    }
+    Process
+    {
+      switch ($Resource){ 
+            "Environment" {$o = New-Object Octopus.Client.Model.EnvironmentResource}
+            "Project" {$o = New-Object Octopus.Client.Model.ProjectResource}            
+            "ProjectGroup" {$o = New-Object Octopus.Client.Model.ProjectGroupResource}            
+        }
+
+    }
+    End
+    {
+        return $o
+    }
+}
