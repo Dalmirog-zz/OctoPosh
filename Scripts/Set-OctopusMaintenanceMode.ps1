26e65cda6f01a43466f65521233105187badfd72
Dalmiro <dalmiro.granias@gmail.com>
2015-02-09 20:53:15 -0300

    Adding Set-OctopusMaintenanceMode
    closes #13

---
+++ Scripts/Set-OctopusMaintenanceMode.ps1
@@ -0,0 +1,48 @@
+﻿<#
+.Synopsis
+   Puts Octopus on/off maintenance mode
+.DESCRIPTION
+   Puts Octopus on/off maintenance mode
+.EXAMPLE
+   Set-OctopusMaintenanceMode -On
+.EXAMPLE
+   Set-OctopusMaintenanceMode -Off
+#>
+function Set-OctopusMaintenanceMode
+{
+    [CmdletBinding()]
+    Param
+    (
+        # Sets Octopus maintenance mode on
+        [Parameter(Mandatory=$true,ParameterSetName='On')]
+        [switch]$On,
+
+        # Sets Octopus maintenance mode off
+        [Parameter(Mandatory=$true,ParameterSetName='Off')]
+        [switch]$Off
+    )
+
+    Begin
+    {
+        $null = New-OctopusConnection #Using this cmdlet just cause it validates that $env:OctopusAPIKey and $env:OctopusURI are populated
+
+        $Header =  @{ "X-Octopus-ApiKey" = $env:OctopusAPIKey }
+    }
+    Process
+    {
+        
+        
+        If ($on){$MaintenanceMode = "True"}
+
+        else {$MaintenanceMode = "False"}
+ 
+        $body = @{IsInMaintenanceMode=$MaintenanceMode} | ConvertTo-Json
+ 
+        $r = Invoke-WebRequest -Uri "$Env:OctopusURI/api/maintenanceconfiguration" -Method PUT -Headers $Header -Body $body
+    }
+    End
+    {
+        If ($r.statuscode -eq 200) {Return $true}
+        else {Return $false}
+    }
+}
