9ccb52618e0407fe7887c32652f8e82c549f2227
Dalmiro <dalmiro.granias@gmail.com>
2015-02-13 05:44:34 -0300

    Added tests
    [+] Get/Set-OctopusConnectionInfo 67ms
    [+] Set-OctopusMaintenanceMode ON/OFF 83ms
    [+] Set-OctopusUserAccountStatus Enabled/Disabled 565ms
    [+] New-OctopusAPIKey creates an API Key 334ms
    
    [+] Environment 756ms
    [+] Project & Project Group 1.85s

--- OctopusDeploy.tests.ps1
+++ OctopusDeploy.tests.ps1
@@ -2,9 +2,9 @@
 
 Describe "Octopus Module Tests" {
 
-        Context "Lovely Tests"{
+        $TestName = new-testname
 
-            $TestName = new-testname
+        Context "Lovely Tests"{            
 
             It "Get/Set-OctopusConnectionInfo" {
             
@@ -40,12 +40,74 @@ Describe "Octopus Module Tests" {
 
             It "New-OctopusAPIKey creates an API Key"{
 
+                #changing WarningPreference to avoid getting the warning of
+                #new-octopusAPIKey so tests look nicer
+                $WarningPreference = "silentlycontinue"
+
                 $api = New-OctopusAPIKey -Purpose "$TestName" -Username Ian.Paullin -password "Michael2"
+
+                #Setting it back to normal
+                $WarningPreference = "continue"
 
                 $api.purpose | should be $TestName
 
                 $api.APIKey | should not be $null
 
+                {$c.repository.Users.RevokeApiKey($api)} | should not throw
+
             }
+        }
+
+        Context "Create/Delete resources"{
+
+            $c = New-OctopusConnection
+
+            It "Environment"{                
+
+                $env = Get-OctopusResourceModel -Resource Environment
+
+                $env.Name = $testname
+
+                $envobj = New-OctopusResource -Resource $env
+
+                $envobj.Name | should be $testname
+                $envobj.Id | should not be $null 
+
+                Remove-OctopusResource -Resource $envobj
+
+                $c.repository.Environments.FindByName($testname) | should be $null
+
+                }
+
+            It "Project & Project Group"{
+
+                $Pg = Get-OctopusResourceModel -Resource ProjectGroup
+                $Proj = Get-OctopusResourceModel -Resource Project
+
+                $Pg.Name = $testname
+
+                $Pgobj = New-OctopusResource -Resource $Pg
+
+                $Pgobj.Name | should be $testname
+                $Pgobj.Id | should not be $null
+
+                $Proj.Name = $testname
+                $Proj.ProjectGroupId = $Pgobj.Id
+                $Proj.LifecycleId = "lifecycle-ProjectGroups-1"
+
+                $Projobj = New-OctopusResource -Resource $Proj
+
+                $Projobj.Name | should be $testname
+                $Projobj.Id | should not be $null
+                
+                Remove-OctopusResource -Resource $Projobj
+                Remove-OctopusResource -Resource $Pgobj
+               
+
+                $c.repository.ProjectGroups.FindByName($testname) | should be $null
+                $c.repository.Projects.FindByName($testname) | should be $null
+
+            }
+        
         }
 }
