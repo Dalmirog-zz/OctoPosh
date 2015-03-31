3f54afa496f4ed56cfb1c20efcccfb8bc77cbb0a
Dalmiro <dalmiro.granias@gmail.com>
2015-03-31 02:43:45 -0300

    Adding New-OctopusResource

---
+++ Scripts/New-OctopusResource.ps1
@@ -0,0 +1,43 @@
+﻿<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function New-OctopusResource
+{
+    [CmdletBinding()]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipeline=$true,
+                   Position=0)]
+        $Resource
+    )
+
+    Begin
+    {
+        $c = New-OctopusConnection
+    }
+    Process
+    {
+        switch ($Resource)
+        {
+            {$_.getType() -eq [Octopus.Client.Model.ProjectGroupResource]} {$c.repository.ProjectGroups.Create($_)}
+            {$_.getType() -eq [Octopus.Client.Model.ProjectResource]} {$c.repository.Projects.Create($_)}
+            {$_.getType() -eq [Octopus.Client.Model.EnvironmentResource]} {$c.repository.Environments.Create($_)}            
+        }
+
+
+    }
+    End
+    {
+    }
+}
+
+
