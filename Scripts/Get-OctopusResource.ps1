c74946142707806f2ede23ebe2a65b672d831bd3
Dalmiro <dalmiro.granias@gmail.com>
2015-04-24 21:07:03 -0300

    Adding Get/Put OctopusResource

---
+++ Scripts/Get-OctopusResource.ps1
@@ -0,0 +1,4 @@
+﻿function Get-OctopusResource([string]$uri, [object]$header) {
+    
+    return Invoke-RestMethod -Method Get -Uri "$env:OctopusURL/$uri" -Headers $header
+}
---
+++ Scripts/Put-OctopusResource.ps1
@@ -0,0 +1,5 @@
+﻿function Put-OctopusResource([string]$uri, [object]$resource) {
+    Write-Host "[PUT]: $uri"
+    #Write-Output $resource | ConvertTo-Json -Depth 10
+    Invoke-RestMethod -Method Put -Uri "$env:OctopusURL/$uri" -Body $($resource | ConvertTo-Json -Depth 10) -Headers $c.header
+}
