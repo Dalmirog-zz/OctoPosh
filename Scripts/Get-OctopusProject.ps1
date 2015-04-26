47ffb84bfa3d048104b810f8da6100a57da8d1aa
Dalmiro <dalmiro.granias@gmail.com>
2015-04-26 00:26:43 -0300

    Help and input from pipeline
    - Improved help
    - Cmdlet now accepts input from pipeline by property name

--- Scripts/Get-OctopusProject.ps1
+++ Scripts/Get-OctopusProject.ps1
@@ -4,33 +4,54 @@
 .DESCRIPTION
    Gets information about Octopus Projects
 .EXAMPLE
-   Example of how to use this cmdlet
+   Get-OctopusProject
+
+   This command gets all the projects of the current Instance
 .EXAMPLE
-   Another example of how to use this cmdlet
+   Get-OctopusProject -name MyProject
+
+   This command gets the project named "MyProject"
+.EXAMPLE
+   Get-OctopusProject -name MyApp*
+
+   This command gets all the projects whose name start with the string MyApp
+.EXAMPLE
+   $environments = Get-OctopusEnvironment -Name "Production"
+
+   $environments.latestdeployment | %{ $_ | Get-OctopusProject}
+
+   This command gets all the projects that deployed to the environment "Production" at least once
+.EXAMPLE
+   $ProjectGroup = Get-OctopusProjectGroup
+
+   $ProjectGroup | Get-OctopusProject | Remove-OctopusResource
+
+   This command gets all the projects inside of a Project Group and then deletes them from the database
 .LINK
    Github project: https://github.com/Dalmirog/OctopusDeploy-Powershell-module
 #>
 function Get-OctopusProject
 {
-    [CmdletBinding()]
-    [Alias()]
+    [CmdletBinding()]    
     Param
     (
-        # Environment name
-        [Parameter(Position=0)]
-        [string[]]$Name = $null,
-
-        # Environment ID
-        [String[]]$ID = $null
+        #Project name
+        [alias("ProjectName")]
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
+        [string[]]$Name
     )
 
     Begin
     {
         $c = New-OctopusConnection
+    }
+    Process
+    {
 
-        If(($Name -ne $null) -or ($ID -ne $null)){
+        #Getting Projects        
+        If(!([string]::IsNullOrEmpty($Name))){
             
-            $Projects = $c.repository.Projects.FindMany({param($Proj) if (($Proj.name -in $name) -or ($Proj.name -like $name) -or ($Proj.id -in $Id)) {$true}})
+            $Projects = $c.repository.Projects.FindMany({param($Proj) if (($Proj.name -in $name) -or ($Proj.name -like $name)) {$true}})
         }
 
         else{
@@ -38,24 +59,48 @@ function Get-OctopusProject
             $Projects = $c.repository.projects.FindAll()
         }        
 
+        #Getting Dashboard info
         $dashboard = Get-OctopusResource "/api/dashboard/dynamic" -header $c.header
-        
-    }
-    Process
-    {
 
-        $list = @()
 
+        $list = @()        
+
+        #Getting info by project
         foreach ($p in $Projects){
 
+            $deployments = @()
+
+            $dashboardItem = $dashboard.Items | ?{$p.Id -eq $_.projectid}
+
+            foreach($d in $dashboardItem){
+                
+                $t = $c.repository.Tasks.Get($d.links.task)
+
+                $dev = (Invoke-WebRequest -Uri "$env:OctopusURL/api/events?regarding=$($d.Id)" -Method Get -Headers $c.header | ConvertFrom-Json).items | ? {$_.category -eq "DeploymentQueued"}
+
+                $dep = [PSCustomObject]@{
+                        ProjectName = ($dashboard.Projects | ?{$_.id -eq $d.projectId}).name
+                        EnvironmentName = ($dashboard.Environments | ?{$_.id -eq $d.EnvironmentId}).name
+                        ReleaseVersion = $d.ReleaseVersion
+                        State = $d.state
+                        CreatedBy = $dev.username
+                        StartTime = ($t.StartTime).datetime
+                        EndTime = ($t.CompletedTime).datetime
+
+                        }
+
+                $deployments += $dep
+            }
             
             $pg = $c.repository.ProjectGroups.Get($p.projectgroupid)
+
             $l = $c.repository.Lifecycles.Get($p.LifeCycleId)
             
             $obj = [PSCustomObject]@{
                 ProjectName = $p.name
-                ProjectGroupName = $pg.name
+                ProjectGroupName = $pg.name                
                 LifecycleName = $l.name
+                LatestDeployments = $deployments
                 AutoCreateRelease = $p.AutoCreateRelease
                 Resource = $p                
             }
@@ -71,5 +116,3 @@ function Get-OctopusProject
         $list
     }
 }
-
-#Get-OctopusProject
