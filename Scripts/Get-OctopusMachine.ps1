d434943eae4aa7b518e948f31df3eab8bb66508e
Dalmiro <dalmiro.granias@gmail.com>
2015-05-12 02:27:17 -0300

    Release Candidate

--- Scripts/Get-OctopusMachine.ps1
+++ Scripts/Get-OctopusMachine.ps1
@@ -8,10 +8,21 @@
 
    Gets the machine with the name "Database_Prod"
 .EXAMPLE
-   .EXAMPLE
-   Get-OctopusMachine -Name "*_Prod"
+    Get-OctopusMachine -Name "*_Prod"
 
-   Gets all the machines which name is like "*_Prod"
+    Gets all the machines which name is like "*_Prod"
+.EXAMPLE
+    Get-OctopusMachine -EnvironmentName "Staging","UAT"
+
+    Gets all the machines on the environments "Staging","UAT"
+.EXAMPLE
+    Get-OctopusMachine -URL "*:10933"
+
+    Gets all the machines with the string "*:10933" at the end of the URL
+.EXAMPLE
+    Get-OctopusMachine -Mode Listening
+
+    Gets all the machines registered in "Listening" mode. "Polling" mode is also available
 .LINK
    Github project: https://github.com/Dalmirog/Octoposh
 #>
@@ -20,19 +31,19 @@ function Get-OctopusMachine
     [CmdletBinding(DefaultParameterSetName="Name")]
     Param
     (
-        # ASK MONSE
+        # ASK MONSE. Make sure to say you cant use 2 wildcarded values
         [Alias("Name")]
         [Parameter(ValueFromPipelineByPropertyName=$true,
                    ParameterSetName = "Name")]
         [string[]]$MachineName,
 
-        # ASK MONSE
+        # ASK MONSE. Make sure to say you cant use 2 wildcarded values
         [Alias("Environment")]
         [Parameter(ValueFromPipelineByPropertyName=$true,
                    ParameterSetName = "Environment")]
         [string[]]$EnvironmentName,
 
-        # ASK MONSE
+        # ASK MONSE. Make sure to say you cant use 2 wildcarded values
         [Alias("URI")]
         [Parameter(ParameterSetName = "URL")]
         [string[]]$URL,
@@ -54,37 +65,112 @@ function Get-OctopusMachine
     }
     Process
     {
-        If(!([string]::IsNullOrEmpty($MachineName)) -or ([string]::IsNullOrEmpty($URL)) -or ([string]::IsNullOrEmpty($CommunicationStyle))){
+        If(($PSCmdlet.ParameterSetName -eq "Name") -and (!([string]::IsNullOrEmpty($MachineName)))) {
+            $Machines = $c.repository.Machines.FindMany({param($Mach) if ((($Mach.name -in $MachineName) -or ($Mach.name -like $MachineName))) {$true}})
 
-            If($CommunicationStyle -eq "Polling"){$CommunicationStyle = "TentacleActive"}            
-            elseIf($CommunicationStyle -eq "Listening"){$CommunicationStyle = "TentaclePassive"}
-            
-            $Machines = $c.repository.Machines.FindMany({param($Mach) if (`
-                        (($Mach.name -in $MachineName) -or ($Mach.name -like $MachineName))`
-                         -or ((($Mach.uri -in $URL) -or ($Mach.uri -like $URL)) -and !([string]::IsNullOrEmpty($mach.uri)))`
-                         -or (($Mach.CommunicationStyle -eq $CommunicationStyle))`                         
-                          ) {$true}})
-            <#
             foreach($n in $MachineName){
-                If($n -notin $Machines.name){
-                    write-host "Machine not found: $n" -ForegroundColor Red
+                        If(($n -notin $Machines.name) -and !($Machines.name -like $n)){
+                            write-error "No Machines found with the name: $n"
+                            #write-host "No Machines found with the name: $n" -ForegroundColor Red
+                        }
+            }
+        }
+
+        elseIf($PSCmdlet.ParameterSetName -eq "CommunicationStyle") {
+
+            If($CommunicationStyle -eq "Polling"){$Style = "TentacleActive"}            
+            elseIf($CommunicationStyle -eq "Listening"){$Style = "TentaclePassive"}
+
+            $Machines = $c.repository.Machines.FindMany({param($Mach) if ($Mach.CommunicationStyle -eq $Style){$true}})
+
+            If($Machines -eq $null){
+                Write-Error "No Machines found with CommunicationStyle: $($Style)"
+                #write-host "No Machines found with CommunicationStyle: $($Style)" -ForegroundColor Red
+            }
+        }
+
+        elseIf(($PSCmdlet.ParameterSetName -eq "URL") -and (!([string]::IsNullOrEmpty($URL)))) {
+            $Machines = $c.repository.Machines.FindMany({param($Mach) if ((($Mach.URI -in $URL) -or ($Mach.URI -like $URL))) {$true}})
+
+            foreach($U in $URL){
+                If(($U -notin $Machines.URI) -and !($Machines.URI -like $URL)){
+                    write-error "No Machines found with the URL: $U"
+                    #write-host "No Machines found with the URL: $U" -ForegroundColor Red
                 }
             }
-            #>
+        }
+
+        elseIf(($PSCmdlet.ParameterSetName -eq "Environment") -and !([string]::IsNullOrEmpty($EnvironmentName))) {
+
+            $machines = @()
+
+            $environments = Get-OctopusEnvironment $EnvironmentName -ResourceOnly
+
+            Foreach($env in $environments){
+
+                $envmachines = $c.repository.Environments.GetMachines($env)
+
+                If($envmachines){
+                    $machines += $envmachines            
+                }
+
+                Else{
+                    Write-Error "No machines were found on Environment: $($env.name)"
+                    #Write-host "No machines were found on Environment: $($env.name)" -ForegroundColor Red
+                }        
+            }    
         }
 
         else{        
             $Machines = $c.repository.Machines.FindAll()
         }
 
-        <#
         If($ResourceOnly){
             $list += $Machines
-        }#>
-        $list += $Machines
+        }
+
+        else{            
+            foreach ($machine in $Machines){        
+                $e = @() 
+
+                If($environments){
+                    $e = $environments | ?{$_.id -eq $machine.EnvironmentIds}
+                }
+
+                Else{
+                    $e = Get-OctopusResource "api/environments/$($machine.EnvironmentIds)" -header $c.header
+                }
+                
+                If($Machine.CommunicationStyle -eq "TentacleActive"){$Style = "Polling"}            
+                If($Machine.CommunicationStyle -eq "TentaclePassive"){$Style = "Listening"}               
+
+                $obj = [PSCustomObject]@{
+                    MachineName = $machine.Name
+                    MachineID = $machine.Id
+                    Thumbprint = $machine.Thumbprint
+                    URI = $machine.uri
+                    IsDisabled = $machine.IsDisabled
+                    EnvironmentName = $e.name
+                    Roles = $machine.Roles
+                    Squid = $machine.Squid
+                    CommunicationStyle = $Style
+                    Status = $machine.Status
+                    StatusSummary = $machine.StatusSummary
+                    LastModifiedOn = $machine.LastModifiedOn
+                    LastModifiedBy = $machine.LastModifiedBy
+                    Resource = $machine
+                }
+
+                $list += $obj
+
+            }
+        }
     }
     End
     {
+        If($list.count -eq 0){
+            $list = $null
+        }
         return $List
     }
 }
