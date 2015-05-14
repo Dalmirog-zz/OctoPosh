08a644fec66c0620a3316571f503474838a7dc8e
Dalmiro <dalmiro.granias@gmail.com>
2015-05-14 03:57:24 -0300

    Working with parameters

--- Scripts/Get-OctopusTask.ps1
+++ Scripts/Get-OctopusTask.ps1
@@ -15,18 +15,23 @@ function Get-OctopusTask
     [CmdletBinding()]
     Param
     (
-        #ID of task
+        #ID of task you want to get
         [Alias("ID")]
         [parameter(ParameterSetName = "TaskId")]
-        [String[]]$TaskID,
+        [String]$TaskID,
         
         # Name of the task
         [ValidateSet("Backup","Delete","Health","Retention","Deploy","Upgrade","AdhocScript","TestEmail")]        
-        [String[]]$Name,
+        [String]$Name = "*",
 
         #Document related to this task.
-        [Alias("RelatedDocumentID")]        
-        [string]$DocumentID,
+        [Alias("DocumentID")]        
+        [string]$ResourceID = "*",
+
+        #Document related to this task.
+        [Alias("Status")]
+        [ValidateSet("Success","TimedOut","Failed","Canceled")]        
+        [string]$State = "*",
 
         #Before date
         [System.DateTimeOffset]$Before = [System.DateTimeOffset]::MaxValue,
@@ -49,14 +54,23 @@ function Get-OctopusTask
             $tasks = $c.repository.Tasks.Get($TaskID)
         }
 
+        elseif(($Name -ne "*") -or ($ResourceID -ne "*") -or ($State -ne "*") -or ($Before -ne [System.DateTimeOffset]::MaxValue) -or ($After -ne [System.DateTimeOffset]::MinValue)) {
+            $tasks = $c.repository.Tasks.FindMany({param($t) if( ($t.name -like $Name)  -and ($t.state -like $State) -and (($t.Arguments.values -contains $ResourceID) -or ($t.Arguments -like $ResourceID)) -and ($t.StartTime -ge $After) -and ($t.LastupdatedTime -le $Before)
+            ) {$true}})        
+        }
+
+        else{
+            $tasks = $c.repository.Tasks.FindAll()
+        }
+
         If($ResourceOnly){
             $list = $tasks
         }
 
         Else{
             Foreach ($task in $tasks){
-
-                $list += $obj
+                $list += $Task
+                #$list += $obj
             }
         }
     }
