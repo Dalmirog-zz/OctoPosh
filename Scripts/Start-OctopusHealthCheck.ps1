fe74f7b264cb9de82f346716e3df2929492f4719
Dalmiro <dalmiro.granias@gmail.com>
2015-05-15 00:49:16 -0300

    Removed support for single machine and adding tests

--- Scripts/Start-OctopusHealthCheck.ps1
+++ Scripts/Start-OctopusHealthCheck.ps1
@@ -1,24 +1,16 @@
 ﻿<#
 .Synopsis
-   Starts a Health Check task on a specific set of machines, or on all the machines on an environment
+   Starts a Health Check task on a specific set environment
 .DESCRIPTION
-   Starts a Health Check task on a specific set of machines, or on all the machines on an environment
-.EXAMPLE
-   Start-OctopusHealthCheck -Machinename "MyProject_Database"
-
-   Starts a health check on the machine "MyProject_Database"
+   Starts a Health Check task on a specific set environment
 .EXAMPLE
    Start-OctopusHealthCheck -Environment "Staging"
 
    Starts a health check on all the machines inside of the environment "Staging"
 .EXAMPLE
-   Get-OctopusEnvironment -Name "Production" | Start-OctopusHealthCheck
+   Get-OctopusEnvironment -Name "Production" | Start-OctopusHealthCheck -force -Message "Health Check from powershell"
 
    Starts a health check on all the machines inside of the environment "Production"
-.EXAMPLE
-   Get-OctopusMachine -Name "MyDb_Prod" | Start-OctopusHealthCheck
-
-   Starts a health check on the machine "MyDb_Prod"
 .LINK
    Github project: https://github.com/Dalmirog/Octoposh
 #>
@@ -27,67 +19,81 @@ function Start-OctopusHealthCheck
     [CmdletBinding()]
     Param
     (
-        # The name of the Machine on which you wanna start a health check. A task will start for each Environment listed on this parameter.        
-        [Parameter(Mandatory=$true,
-                   ValueFromPipelineByPropertyName=$true,
-                   ParameterSetNAme="Machine")]
-        [string[]]$MachineName,
-
         # The name of the environment/s on which you wanna start a health check. A task will start for each Environment listed on this parameter
         [Parameter(Mandatory=$true,
-                   ValueFromPipelineByPropertyName=$true,
-                   ParameterSetNAme="Environment")]
+                   ValueFromPipelineByPropertyName=$true)]
         [string[]]$EnvironmentName,
 
         #The message that will show up on the Octopus task
         [string]$Message,
 
-        # Forces resource delete.
-        [switch]$Force
+        # Forces health check task start.
+        [switch]$Force,
+
+        # Waits until the task is not on states "Queued" or "Executing"
+        [switch]$Wait,
+
+        # Timeout for -Wait parameter in minutes. Default timeout is 2 minutes
+        [double]$Timeout = 2
     )
 
     Begin
     {
         $c = New-OctopusConnection
+        $list = @()
     }
     Process
     {
-        If($PSCmdlet.ParameterSetName -eq "Environment"){            
-            $targets = Get-OctopusMachine -EnvironmentName $EnvironmentName -ResourceOnly
-        }
+        foreach ($environment in $EnvironmentName){
+            
+            $Machines = Get-OctopusMachine -EnvironmentName $Environment -ResourceOnly
+
+            If($Machines -ne $null){
+                
+                If(!$message){
+                    $message = "[API Generated] Check Health on Environment: $Environment"
+                }
+
+                If(!($Force)){
+                    If (!(Get-UserConfirmation -message "Are you sure you want to start a health check on the environment: $Environment")){
+                        Throw 'Canceled by user'
+                    }
+                }
         
-        else{$targets = Get-OctopusMachine -Name $machinename -ResourceOnly}
-        
-        If($targets -eq $null){
-            Throw "No machines where found "
-        }      
+                Write-Verbose "Starting Health check on Environment $environment which contains machines:"
+                $Machines.name | %{Write-Verbose $_}
 
-        If(!($Force)){
-            If (!(Get-UserConfirmation -message "Are you sure you want to start a health check on the $($PSCmdlet.ParameterSetName): $($target.name)")){
-                Throw "Canceled by user"
+                $EnvironmentID = $Machines[0].environmentIDs[0]
+
+                $Task = $c.repository.Tasks.ExecuteHealthCheck($Message,5,$environmentId,$Machines.Id)        
+
+                If($wait){
+
+                    $StartTime = Get-Date
+
+                    Do{
+                        $CurrentTime = Get-date
+                    
+                        $task = Get-OctopusTask -ID $task.id -ResourceOnly
+                    
+                        Start-Sleep -Seconds 2
+                    }Until (($task.state -notin ('Queued','executing')) -or ($CurrentTime -gt $StartTime.AddMinutes($Timeout)))
+
+                    Write-Verbose "Health task on environment $environment finished with status: $($task.state.tostring().toupper())"
+                }
+                $list += $Task
+            }
+
+            else{
+                Write-Error 'No machines were found'
             }
         }
-
-        foreach ($target in $targets){
-
-            If(!$message){
-                $message = "[API Generated] Check Health on $($PSCmdlet.ParameterSetName): $($target.name)"
-            }
-
-            Write-Verbose "Starting Health check on $($PSCmdlet.ParameterSetName): $($target.name)"
-
-            If($PSCmdlet.ParameterSetName -eq "Environment"){
-               $task = $c.repository.Tasks.ExecuteHealthCheck($Message,5,$target.id)
-            }
-
-            Else{
-               #INVESTIGAR $Target.Links.connection
-               $task = $c.repository.Tasks.ExecuteHealthCheck($Message,5,($target | select -ExpandProperty Environmentids | select -First 1),$target.Id)
-            }
-        }
-    }
+    }   
     End
     {
-        return $task
+        If($list.count -eq 0){
+            $list = $null
+        }
+        return $List
     }
 }
