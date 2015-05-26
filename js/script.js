fcfa84cf99ad989d2f203e9e26c3e540fab5f95c
Anabella Spinelli <anabellaspinelli@gmail.com>
2015-05-26 10:43:09 -0300

    Reading latest version from OctoPosh json file. Only on Jumbotron.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 var version = document.getElementById("version");
 
 $(document).ready(function() {
-	$.getJSON("https://api.mercadolibre.com/sites/MLA/search?q=bicicleta+de+paseo+rod+28", function(response) {
-		version.innerHTML = "v." + response.paging.total;
+	$.getJSON("https://api.github.com/repos/dalmirog/OctoPosh/contents/SiteInfo.json?ref=development", function(response) {
+		var versions = JSON.parse(window.atob(response.content).substr(3));
+		var latest = versions.Items.length - 1;
+		version.innerHTML = "v." + versions.Items[latest].Version;
 	});
 });
