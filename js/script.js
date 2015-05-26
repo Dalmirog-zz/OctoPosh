987ee96ec425eb116bf21a3a794b8276e2ac540d
Anabella Spinelli <anabellaspinelli@gmail.com>
2015-05-25 21:04:57 -0300

    Back to MercadoLibre API.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var version = document.getElementById("version");
 
 $(document).ready(function() {
-	$.getJSON("https://raw.githubusercontent.com/anabellaspinelli/OctoPosh/gh-pages/version.json", function(response) {
-		version.innerHTML = "v." + response.version.number;
+	$.getJSON("https://api.mercadolibre.com/sites/MLA/search?q=bicicleta+de+paseo+rod+28", function(response) {
+		version.innerHTML = "v." + response.paging.total;
 	});
 });
