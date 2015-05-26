9d8dac99e7b16d7a1418ce39862be1820edb9ed4
Anabella Spinelli <anabellaspinelli@gmail.com>
2015-05-26 17:07:06 -0300

    Adding ZeroClipboard. Test.

--- js/script.js
+++ js/script.js
@@ -9,6 +9,23 @@ $(document).ready(function() {
 	$.getJSON("https://api.github.com/repos/dalmirog/OctoPosh/releases", function(response) {
 		getAndShowLatestVersion(response);
 	});
+	//ZeroClipboard.config( { moviePath: "https://cdnjs.cloudflare.com/ajax/libs/zeroclipboard/2.2.0/ZeroClipboard.swf" } );
+
+	var client = new ZeroClipboard( document.getElementById("copy-button"), {
+		moviePath: "/js/ZeroClipboard.swf"
+	} );
+
+	client.on( "ready", function( readyEvent ) {
+	   alert( "ZeroClipboard SWF is ready!" );
+
+	  client.on( "aftercopy", function( event ) {
+	    // `this` === `client`
+	    // `event.target` === the element that was clicked
+	    event.target.style.display = "none";
+	    alert("Copied text to clipboard: " + event.data["text/plain"] );
+	  } );
+	} );
 });
 
 // GET ALL RELEASES /repos/:owner/:repo/releases/
+
