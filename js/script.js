9c5074bd3a3b0c2ca40a8e361e095d021956e35e
Anabella Spinelli <anabellaspinelli@gmail.com>
2015-05-27 01:41:40 -0300

    Adding dynamic table for versions with a mocked JSON of the actual GitHub API's versions. Adding favicons. Adding underscoreJS for templating.

--- js/script.js
+++ js/script.js
@@ -1,27 +1,54 @@
+var githubReleases;
 
-function getAndShowLatestVersion(versions) {
-	var version = document.getElementById("version");
-	var latest = versions.length - 1;
-	version.innerHTML = versions[latest].name;
+function getReleases() {
+	$.getJSON("https://api.github.com/repos/dalmirog/OctoPosh/releases", function(response) {
+		githubReleases = response;
+		displayLatestRelease();
+		populateReleasesTable();
+	});
 }
 
-$(document).ready(function() {
-	$.getJSON("https://api.github.com/repos/dalmirog/OctoPosh/releases", function(response) {
-		getAndShowLatestVersion(response);
-	});
-	//ZeroClipboard.config( { moviePath: "https://cdnjs.cloudflare.com/ajax/libs/zeroclipboard/2.2.0/ZeroClipboard.swf" } );
+function displayLatestRelease() {
+	var version = document.getElementById("version");
+	var latest = githubReleases.length - 1;
+	version.innerHTML = githubReleases[latest].name;	
+}
 
+function initZeroClipboard() {
 	var client = new ZeroClipboard( document.getElementById("copy-button"), {
 		moviePath: "/js/ZeroClipboard.swf"
 	});
 
-	client.on( "aftercopy", function( event ) {
+	client.on("aftercopy", function(event) {
 	    $("#copy-button > span").removeClass("glyphicon-copy");
 	    $("#copy-button > span").addClass("glyphicon-ok");
 	    $("#copy-button").removeClass("btn-primary");
 	    $("#copy-button").addClass("btn-success");
-	  } );
+	  });
+}
+
+
+function createReleasesJSON() {
+	var releasesJSON = [];
+	for (var i = 0; i <= 4; i++) {
+		releasesJSON[i] = {};
+		releasesJSON[i].name = "OctoPosh " + githubReleases[0].name; //Change [0] for something nice when I have more than one GH release.
+		releasesJSON[i].date = githubReleases[0].published_at;
+		releasesJSON[i].url = githubReleases[0].body;
+	};
+	return releasesJSON;
+}
+
+function populateReleasesTable() {
+	JSON = createReleasesJSON();
+	console.log(JSON);
+	var template = _.template($("#releases-template").html());
+	JSON.forEach(function(release) {
+		$("#releases > tbody").append(template(release));
+	});
+}
+
+$(document).ready(function() {
+	getReleases();
+	initZeroClipboard();
 });
-
-// GET ALL RELEASES /repos/:owner/:repo/releases/
-
