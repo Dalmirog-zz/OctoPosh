a92c2d528891bbbc4f187c96a3e3f2e5086462ac
Anabella Spinelli <anabellaspinelli@gmail.com>
2015-05-26 21:43:58 -0300

    Changing copy icon when operation is successful.

--- js/script.js
+++ js/script.js
@@ -16,8 +16,8 @@ $(document).ready(function() {
 	});
 
 	client.on( "aftercopy", function( event ) {
-	    $("#copy-button").removeClass("glyphicon-copy");
-	    $("#copy-button").addClass("glyphicon-ok");
+	    $("#copy-button").removeClass("glyphicon-copy btn-primary");
+	    $("#copy-button").addClass("glyphicon-ok btn-success");
 	    alert("Copied text to clipboard: " + event.data["text/plain"] );
 	  } );
 });
