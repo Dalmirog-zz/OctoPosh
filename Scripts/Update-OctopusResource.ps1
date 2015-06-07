220eee4b00d109f30e856c5114d47dbdb6ec5f72
Dalmiro <dalmiro.granias@gmail.com>
2015-06-06 21:07:29 -0300

    Added Script and tests

---
+++ Scripts/Update-OctopusResource.ps1
@@ -0,0 +1,92 @@
+﻿<#
+.Synopsis
+   
+
+   This is an advanced cmdlet and all its examples involve multiple lines of code. Please check the advanced examples for a better reference: https://github.com/Dalmirog/OctoPosh/wiki/Advanced-Examples
+.DE
+   Deletes Octopus Deploy resources such as Projects, Releases, Environments, etc
+
+   This is an advanced cmdlet and all its examples involve multiple lines of code. Please check the advanced examples for a better reference: https://github.com/Dalmirog/OctoPosh/wiki/Advanced-Examples
+.EXAMPLE
+   
+.EXAMPLE
+   
+.LINK
+   Github project: https://github.com/Dalmirog/Octoposh
+   Advanced Cmdlet Usage: https://github.com/Dalmirog/OctoPosh/wiki/Advanced-Examples
+   QA and Cmdlet request: https://gitter.im/Dalmirog/OctoPosh#initial
+#>
+function Update-OctopusResource
+{
+    [CmdletBinding(DefaultParameterSetName='Update')]
+    Param
+    (
+        # Octopus resource object
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   ParameterSetName = 'Update',                   
+                   Position=0)]
+        [object[]]$Resource,
+
+        # Prints a list of the resource types accepted by the parameter $Resource.
+        [parameter(ParameterSetName = 'ListAcceptedTypes')]
+        [switch]$AcceptedTypes,
+        
+        # Forces cmdlet to continue without prompting
+        [switch]$Force
+    )
+
+    Begin
+    {
+        If($AcceptedTypes){
+            'Octopus.Client.Model.ProjectGroupResource'
+            'Octopus.Client.Model.ProjectResource'
+            'Octopus.Client.Model.EnvironmentResource'
+            'Octopus.Client.Model.DeploymentResource'
+            'Octopus.Client.Model.MachineResource'
+            'Octopus.Client.Model.FeedResource'
+            'Octopus.Client.Model.LibraryVariableSetResource'            
+        }
+
+        $c = New-OctopusConnection        
+    }
+    Process
+    {
+        Foreach ($r in $Resource){
+
+            if(!($Force)){
+                If (!(Get-UserConfirmation -message "Are you sure you want to update this resource? `n`n [$($R.GetType().tostring())] $($R.name)`n")){
+                    Throw 'Canceled by user'
+                }
+            }
+            switch ($r)
+            {
+                {$_.getType() -eq [Octopus.Client.Model.ProjectGroupResource]} {$ResourceType = 'ProjectGroups'}
+                {$_.getType() -eq [Octopus.Client.Model.ProjectResource]} {$ResourceType = 'Projects'}
+                {$_.getType() -eq [Octopus.Client.Model.EnvironmentResource]} {$ResourceType = 'Environments'}
+                {$_.getType() -eq [Octopus.Client.Model.DeploymentResource]} {$ResourceType = 'Deployments'}
+                {$_.getType() -eq [Octopus.Client.Model.MachineResource]} {$ResourceType = 'Machines'}          
+                {$_.getType() -eq [Octopus.Client.Model.FeedResource]} {$ResourceType = 'Feeds'}
+                {$_.getType() -eq [Octopus.Client.Model.VariableSetResource]} {$ResourceType = 'LibraryVariableSets'}
+                Default{Throw "Invalid object type: $($_.getType()) `nRun 'Update-OctopusResource -AcceptedTypes' to get a list of the object types accepted by this cmdlet"}                
+            }
+
+            Write-Verbose "[$($MyInvocation.MyCommand)] Updating $ResourceType`: $($r.name)"
+
+            Try{
+                $UpdatedObject = $c.repository.$ResourceType.Modify($r)
+            }
+            Catch{
+                Write-Error $_    
+            }
+        }
+    }
+    End
+    {
+        If(!$UpdatedObject){
+            $UpdatedObject = $null
+        }
+
+        return $UpdatedObject
+    }
+}
