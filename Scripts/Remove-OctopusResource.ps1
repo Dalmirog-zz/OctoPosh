0b793ae9a3b78500c7b50ccad06d94b27e491fda
Dalmiro <dalmiro.granias@gmail.com>
2015-11-03 01:20:59 -0300

    Revert "Revert "remove -wait parameter and task loop""
    This reverts commit d599bd8c2e554a7536655dd9461b79e1105f49af.

--- Scripts/Remove-OctopusResource.ps1
+++ Scripts/Remove-OctopusResource.ps1
@@ -37,13 +37,7 @@ function Remove-OctopusResource
         [switch]$AcceptedTypes,
 
         # Forces cmdlet to continue without prompting
-        [switch]$Force,
-
-        # Waits until the task is not on states "Queued" or "Executing"
-        [switch]$Wait,
-
-        # Timeout for [Wait] parameter in minutes
-        [double]$Timeout = 2
+        [switch]$Force
     )
 
     Begin
@@ -83,44 +77,29 @@ function Remove-OctopusResource
             }
 
             Write-Verbose "[$($MyInvocation.MyCommand)] Deleting [$($R.GetType().tostring())] $($r.name)"
-
-            If(($ResourceType -eq 'Feeds') -or ($ResourceType -eq 'Machines')){
-                #Deleting using REST API cause client library delete method doesnt work
-                $task = (Invoke-WebRequest ($env:OctopusURL + $r.Links.Self) -Method Delete -Headers $c.header).content | ConvertFrom-Json
+            Try{
+                If(($ResourceType -eq 'Feeds') -or ($ResourceType -eq 'Machines')){
+                    #Deleting using REST API cause client library delete method doesnt work
+                    $task = (Invoke-WebRequest ($env:OctopusURL + $r.Links.Self) -Method Delete -Headers $c.header).content | ConvertFrom-Json
+                }
+                elseif($ResourceType -eq 'LibraryVariableSets'){
+                    #Deleting using REST API cause client library delete method doesnt work
+                    $task = Invoke-RestMethod ($env:OctopusURL + '/api/LibraryVariableSets/' +$r.OwnerId) -Method Delete -Headers $c.header
+                }
+                else{
+                    $task = $c.repository.$ResourceType.Delete($r)
+                }
             }
-            elseif($ResourceType -eq 'LibraryVariableSets'){
-                #Deleting using REST API cause client library delete method doesnt work
-                $task = Invoke-RestMethod ($env:OctopusURL + '/api/LibraryVariableSets/' +$r.OwnerId) -Method Delete -Headers $c.header
+            Catch{
+                #If task fails, it'll return the error
+                return $_
             }
-            else{
-                $task = $c.repository.$ResourceType.Delete($r)
-            }
-
-            If($wait){
-
-                $StartTime = Get-Date
-
-                Do{
-                    $CurrentTime = Get-date
-                    
-                    $task = Get-OctopusTask -ID $task.id
-
-                    Write-Verbose "[$($MyInvocation.MyCommand)] Task $($Task.id) status: $($task.state)"
-                    
-                    Start-Sleep -Seconds 2
-                  }
-
-                Until (($task.state -notin ('Queued','executing')) -or ($CurrentTime -gt $StartTime.AddMinutes($Timeout)))
-                Write-Verbose "[$($MyInvocation.MyCommand)] Task finished with status: $($task.state.tostring().toupper())"
-            }            
+            
         }
     }
     End
     {
-        If(!$Task){
-            $Task = $null
-        }
-
-        return $Task
+        #If task starts it'll return true.
+        return $true
     }
 }
