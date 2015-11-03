1b053ecf2cd037cbac0e9d7e1f3ba46a7809d322
Dalmiro <dalmiro.granias@gmail.com>
2015-11-03 01:33:10 -0300

    adding workingtests

---
+++ workingtests.ps1
@@ -0,0 +1,103 @@
+﻿#Generates a random test name that'll be used to name everything on the tests
+Function New-TestName {    
+    
+    $length = 10 #length of random chars
+    $characters = 'abcdefghkmnprstuvwxyzABCDEFGHKLMNPRSTUVWXYZ1234567890' #characters to use
+    
+    # select random characters
+    $random = 1..$length | ForEach-Object { Get-Random -Maximum $characters.length }
+        
+    #Set ofs to "" to avoid having spaces between each char
+    $private:ofs=''
+
+    #output prefix (max 10 chars) + 5 random chars
+    Return [String]($prefix + $characters[$random])
+
+}
+
+Describe 'Octopus Module Tests' {
+
+    $TestName = new-testname
+
+    $c = New-OctopusConnection
+    
+It '[New-OctopusResource] creates environments'{               
+
+        $env = Get-OctopusResourceModel -Resource Environment                
+
+        $env.Name = $testname
+                
+        $envobj = New-OctopusResource -Resource $env
+
+        $envobj.name | should be $testname
+
+    }
+    It '[New-OctopusResource] creates Project Groups'{
+        $Pg = Get-OctopusResourceModel -Resource ProjectGroup
+                                                
+        $Pg.Name = $testname
+
+        $Pgobj = New-OctopusResource -Resource $Pg
+
+        $Pgobj.name | should be $testname
+    }
+    It '[New-OctopusResource] creates Projects'{
+        $Proj = Get-OctopusResourceModel -Resource Project
+                
+        $Proj.Name = $testname
+        $Proj.ProjectGroupId = (Get-OctopusProjectGroup -Name $TestName).id
+        $Proj.LifecycleId = (Get-OctopusLifeCycle)[0].id
+
+        $Projobj = New-OctopusResource -Resource $Proj
+
+        $Projobj.Name | should be $testname
+    }
+    It '[New-OctopusResource] adds NuGet feeds'{
+        $Feedname = $testname
+        $feedURL = "https://$testname.com"
+
+        $feed = Get-OctopusResourceModel -Resource NugetFeed
+
+        $feed.Name = $Feedname
+        $feed.FeedUri = $feedURL
+
+        $newfeed = New-OctopusResource -Resource $feed
+
+        $newfeed.name | should be $testname 
+        $newfeed.feeduri | should be $feedURL
+    }
+    It '[New-OctopusResource] creates Library Variable Sets'{
+        $libraryName = $testname
+        $library = Get-OctopusResourceModel -Resource LibraryVariableSet
+
+        $library.Name = $libraryName
+
+        $NewLibrary = New-OctopusResource -Resource $library
+
+        $NewLibrary.name | should be $testname         
+    }
+
+    It '[Remove-OctopusResource] deletes environments'{                
+        {Get-OctopusEnvironment -Name $testname | Remove-OctopusResource -Force} | should not Throw               
+
+        Get-OctopusEnvironment -Name $TestName -ErrorAction SilentlyContinue | should be $null
+    }        
+    It '[Remove-OctopusResource] deletes Projects'{
+        {Get-OctopusProject -Name $TestName | Remove-OctopusResource -Force} | should not throw
+
+        Get-OctopusProject -Name $TestName -ErrorAction SilentlyContinue| should be $null
+    }
+    It '[Remove-OctopusResource] deletes Project Groups'{
+        {Get-OctopusProjectGroup -Name $TestName |Remove-OctopusResource -Force} | should not throw
+
+        Get-OctopusProjectGroup -Name $TestName -ErrorAction SilentlyContinue | should be $null
+    }
+    It '[Remove-OctopusResource] deletes NuGet feeds'{
+        (Get-OctopusFeed -FeedName $TestName | Remove-OctopusResource -Force) | should be $true
+    }
+    It '[Remove-OctopusResource] deletes Library Variable Sets'{
+        (Get-OctopusVariableSet -LibrarySetName $TestName | Remove-OctopusResource -Force) | should be $true        
+    }
+
+
+}
