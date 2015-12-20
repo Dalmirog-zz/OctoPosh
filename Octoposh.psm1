05541ea77a4cde8800413c821b1e400f295fc4e9
Dalmiro <dalmiro.granias@gmail.com>
2015-12-20 20:19:36 -0300

    Added 3.0 dlls

--- Octoposh.psm1
+++ Octoposh.psm1
@@ -2,7 +2,6 @@ $scripts = Get-ChildItem $PSScriptRoot\scripts -Filter "*.ps1"
 
 Add-Type -Path "$PSScriptRoot\bin\Newtonsoft.Json.dll"
 Add-Type -Path "$PSScriptRoot\bin\Octopus.Client.dll"
-Add-Type -Path "$PSScriptRoot\bin\Octopus.Platform.dll"
 
 foreach ($script in $scripts){
 . $script.FullName
