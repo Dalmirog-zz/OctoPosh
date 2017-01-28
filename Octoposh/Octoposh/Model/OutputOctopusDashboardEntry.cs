ae64d4877a6300eaf64c567772e8542f53b5a291
dalmirog <dalmiro.granias@gmail.com>
2017-01-28 19:15:18 -0300

    First working draft. Still missing tests

---
+++ Octoposh/Octoposh/Model/OutputOctopusDashboardEntry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Octoposh.Model
+{
+    class OutputOctopusDashboardEntry
+    {
+        public string ProjectName { get; set; }
+        public string EnvironmentName { get; set; }
+        public string ReleaseVersion { get; set; }
+        public string DeploymentStatus { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsCompleted { get; set; }
+        public bool HasPendingInterruptions { get; set; }
+        public bool HasWarninsOrErrors { get; set; }
+        public string Duration { get; set; }
+    }
+}
