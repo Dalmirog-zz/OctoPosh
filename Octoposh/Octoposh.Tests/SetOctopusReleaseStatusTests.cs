71889c417a0b40b118517b82859237247e9fd9f9
dalmirog <dalmiro.granias@gmail.com>
2017-03-14 21:56:48 -0300

    Adding SetOctopusReleaseStatus cmdlet + tests

---
+++ Octoposh/Octoposh.Tests/SetOctopusReleaseStatusTests.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using NUnit.Framework;
+using Octoposh.Cmdlets;
+using Octoposh.Model;
+using Octopus.Client;
+using Octopus.Client.Model;
+
+namespace Octoposh.Tests
+{
+    [TestFixture]
+    public class SetOctopusReleaseStatusTests
+    {
+        //If sure this mess of initialization variables can be much simpler. But its 10pm and it works :). We accept PRs!
+        private static readonly string CmdletName = "Set-OctopusReleaseStatus";
+        private static readonly Type CmdletType = typeof(SetOctopusReleaseStatus);
+        private static readonly string ProjectName = "ReleaseTests_Project1";
+
+        private static readonly string Server = string.Concat("http://localhost:", ConfigurationManager.AppSettings["OctopusBindingPort"]);
+        private static readonly string Apikey = ConfigurationManager.AppSettings["OctopusAPIKey"];
+        private static readonly OctopusServerEndpoint Endpoint = new OctopusServerEndpoint(Server, Apikey);
+        private static readonly OctopusRepository Repository = new OctopusRepository(Endpoint);
+
+        private static readonly ProjectResource Project = Repository.Projects.FindByName(ProjectName);
+        private static readonly string ReleaseVersion = "0.0.1";
+        private static readonly ReleaseResource Release = Repository.Projects.GetReleaseByVersion(Project, ReleaseVersion);
+
+        private static readonly string Blocked = "Blocked";
+        private static readonly string Unblocked = "Unblocked";
+        private static readonly string Description = $"Raising defect from unit tests on [{DateTime.Now}]";
+
+        private const string ByReleaseResource = "ByReleaseResource";
+        private const string ByProjectAndVersion = "ByProjectAndVersion";
+
+        [Test]
+        public void BlockAndUnblockReleaseByProjectNameAndVersion()
+        {
+            var latestDefect = Repository.Defects.GetDefects(Release).Items.LastOrDefault();
+
+            //If the release doesn't have any defects or the last one has the status "Resolved" (meaning its not blocked) => Block first and unblock later.
+            if (latestDefect == null || latestDefect.Status.ToString() == "Resolved")
+            {
+                Assert.IsTrue(InvokeCmdlet(Release, Blocked, ByProjectAndVersion));
+                Assert.IsTrue(InvokeCmdlet(Release, Unblocked, ByProjectAndVersion));
+            }
+            //else Unblock first, and block later.
+            else
+            {
+                Assert.IsTrue(InvokeCmdlet(Release, Unblocked, ByProjectAndVersion));
+                Assert.IsTrue(InvokeCmdlet(Release, Blocked, ByProjectAndVersion));
+            }
+        }
+
+        [Test]
+        public void BlockAndUnblockReleaseByReleaseResource()
+        {
+            var latestDefect = Repository.Defects.GetDefects(Release).Items.LastOrDefault();
+
+            //If the release doesn't have any defects or the last one has the status "Resolved" (meaning its not blocked) => Block first and unblock later.
+            if (latestDefect == null || latestDefect.Status.ToString() == "Resolved")
+            {
+                Assert.IsTrue(InvokeCmdlet(Release, Blocked, ByReleaseResource));
+                Assert.IsTrue(InvokeCmdlet(Release, Unblocked, ByReleaseResource));
+            }
+
+            //else Unblock first, and block later.
+            else
+            {
+                Assert.IsTrue(InvokeCmdlet(Release, Unblocked, ByReleaseResource));
+                Assert.IsTrue(InvokeCmdlet(Release, Blocked, ByReleaseResource));
+            }
+        }
+
+        private bool InvokeCmdlet(ReleaseResource release,string status,string parameterSet)
+        {
+            List<CmdletParameter> parameters;
+
+            if (parameterSet == ByProjectAndVersion)
+            {
+                parameters = new List<CmdletParameter>
+                {
+                    new CmdletParameter()
+                    {
+                        Name = "ProjectName",
+                        SingleValue = ProjectName
+                    },
+                    new CmdletParameter()
+                    {
+                        Name = "ReleaseVersion",
+                        SingleValue = ReleaseVersion
+                    },
+                    new CmdletParameter()
+                    {
+                        Name = "Status",
+                        SingleValue = status
+                    }
+                    ,
+                    new CmdletParameter()
+                    {
+                        Name = "Description",
+                        SingleValue = Description
+                    }
+                };
+            }
+            else
+            {
+                parameters = new List<CmdletParameter>
+                {
+                    new CmdletParameter()
+                    {
+                        Name = "Resource",
+                        Resource = Release
+                    },
+                    new CmdletParameter()
+                    {
+                        Name = "Status",
+                        SingleValue = status
+                    }
+                    ,
+                    new CmdletParameter()
+                    {
+                        Name = "Description",
+                        SingleValue = Description
+                    }
+                };
+            }
+
+            var powershell = new CmdletRunspace().CreatePowershellcmdlet(CmdletName, CmdletType, parameters);
+
+            try
+            {
+                powershell.Invoke<List<OutputOctopusTeam>>();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Octoposh/Octoposh/Cmdlets/SetOctopusReleaseStatus.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Octoposh.Model;
+using Octopus.Client.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Management.Automation;
+
+namespace Octoposh.Cmdlets
+{
+    /// <summary>
+    /// <para type="Description">Blocks or unblocks a release or set of releases.</para>
+    /// </summary>
+    /// <summary>
+    /// <para type="Description">Blocks or unblocks a release or set of releases.</para>
+    /// </summary>
+    /// <example>   
+    ///   <code>PS C:\> Set-OctopusReleaseStatus -ProjectName MyProject -ReleaseVersion 1.0.0 -Description "Because of reasons"</code>
+    ///   <para>Blocks the release [1.0.0] from the project [MyProject] from being deployed with the reson ["Because of reasons"]. Using the "ProjectName" parameter allows you to only block releases in one project at a time. For multiple releases check usage of parameter "Resource"</para>    
+    /// </example>
+    /// <example>   
+    ///   <code>PS C:\> Set-OctopusReleaseStatus -ProjectName MyProject -ReleaseVersion 1.0.0, 2.0.0 -Description "Because of reasons"</code>
+    ///   <para>Blocks the releasse [1.0.0],[2.0.0] from the project [MyProject] from being deployed with the reson ["Because of reasons"]. Using the "ProjectName" parameter allows you to only block releases in one project at a time. For multiple releases check usage of parameter "Resource"</para>    
+    /// </example>
+    /// <example>   
+    ///   <code>PS C:\> Set-OctopusReleaseStatus -Resource $ReleaseResource -Description </code>
+    ///   <para>Blocks all the releases</para>
+    /// </example>
+    /// <para type="link" uri="http://Octoposh.net">WebSite: </para>
+    /// <para type="link" uri="https://github.com/Dalmirog/OctoPosh/">Github Project: </para>
+    /// <para type="link" uri="https://github.com/Dalmirog/OctoPosh/wiki">Wiki: </para>
+    /// <para type="link" uri="https://gitter.im/Dalmirog/OctoPosh#initial">QA and Feature requests: </para>
+    [Cmdlet("Set", "OctopusReleaseStatus")]
+    public class SetOctopusReleaseStatus : PSCmdlet
+    {
+        private const string ByReleaseResource = "ByReleaseResource";
+        private const string ByProjectAndVersion = "ByProjectAndVersion";
+
+        /// <summary>
+        /// <para type="description">Project name</para>
+        /// </summary>
+        [ValidateNotNullOrEmpty()]
+        [Parameter(Mandatory = true, Position = 1, ValueFromPipeline = true, ParameterSetName = ByProjectAndVersion)]
+        public string ProjectName { get; set; }
+
+        /// <summary>
+        /// <para type="description">Releases to Block/Unblock</para>
+        /// </summary>
+        [ValidateNotNullOrEmpty()]
+        [Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true, ParameterSetName = ByProjectAndVersion)]
+        public List<string> ReleaseVersion { get; set; }
+
+        /// <summary>
+        /// <para type="description">Reason to block the deployment</para>
+        /// </summary>
+        [Alias("Reason")]
+        [Parameter(Position = 3)]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// <para type="description">Status that the release will be put into</para>
+        /// </summary>
+        [Alias("State")]
+        [ValidateSet("Unblocked", "Blocked")]
+        [Parameter(Mandatory = true, Position = 2)]
+        public string Status { get; set; }
+
+        /// <summary>
+        /// <para type="description">List of [Octopus.Model.ReleaseResource] objects that will get blocked/unblocked. By using this parameter you do not need to pass values to "ProjectName" or "ReleaseVersion", as that info will already be available in the Release object</para>
+        /// </summary>
+        [Parameter(Mandatory = true, Position = 1, ValueFromPipeline = true, ParameterSetName = ByReleaseResource)]
+        public List<ReleaseResource> Resource { get; set; }
+
+        private OctopusConnection _connection;
+
+        protected override void BeginProcessing()
+        {
+            _connection = new NewOctopusConnection().Invoke<OctopusConnection>().ToList()[0];
+            if (string.IsNullOrWhiteSpace(Description))
+            {
+                Description = $"Blocking release from Octoposh on {DateTime.Now}";
+            }
+
+        }
+
+        protected override void ProcessRecord()
+        {
+            var releases = new List<ReleaseResource>();
+
+            if (ParameterSetName == ByProjectAndVersion)
+            {
+                var project = _connection.Repository.Projects.FindByName(ProjectName);
+
+                foreach (var version in ReleaseVersion)
+                {
+                    releases.Add(_connection.Repository.Projects.GetReleaseByVersion(project, version));
+                }
+            }
+            else
+            {
+                releases = Resource;
+            }
+
+            foreach (var release in releases)
+            {
+                if (Status == "Unblocked")
+                {
+                    WriteDebug($"Unblocking release [{release.Version}]");
+                    _connection.Repository.Defects.ResolveDefect(release);
+                }
+                else
+                {
+                    WriteDebug($"Blocking release [{release.Version}]");
+                    _connection.Repository.Defects.RaiseDefect(release, Description);
+                }
+
+            }
+
+        }
+    }
+}
