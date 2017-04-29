537697bfbbcd0941b006464238cdc1c95725c318
dalmirog <dalmiro.granias@Gmail.com>
2017-04-29 03:21:39 -0300

    adding barebones website

---
+++ Octoposh.Website/README.md
@@ -0,0 +1,2 @@
+# JSDevEnv
+Javascript development environment for Pluralsight course https://app.pluralsight.com/player?course=javascript-development-environment
---
+++ Octoposh.Website/buildScripts/startMessage.js
@@ -0,0 +1,4 @@
+//var chalk = require('chalk');
+import chalk from 'chalk';
+
+console.log(chalk.green('starting app in dev mode...')); // eslint-disable-line no-console
