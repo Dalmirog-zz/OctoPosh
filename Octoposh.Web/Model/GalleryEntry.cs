ee32e3407f74c5c0f049952ed01ffb5371f934eb
dalmirog <dalmiro.granias@gmail.com>
2017-08-20 03:43:33 -0300

    Adding initial working version of the website

---
+++ Octoposh.Web/Model/GalleryEntry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Octoposh.Web.Model 
+{
+    public class GalleryEntry : IGalleryEntry
+    {
+       public Version Version { get; set; }
+       public int Downloads { get; set; }
+       public DateTime DatePushed { get; set; }
+       public Uri ReleaseNotesURI {
+            get
+            {
+                var version = Version.ToString().Replace(".", "");
+                var URI = new Uri(String.Format("{0}/{1}", "https://github.com/dalmirog/Octoposh/wiki/Release-Notes#",version));
+                return URI;
+            }
+            set { }
+        }
+        public Uri GalleryDownloadURI { get; set; }
+
+    }
+}
---
+++ Octoposh.Web/Model/IGalleryEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Octoposh.Web.Model
+{
+    public interface IGalleryEntry
+    {
+        Version Version { get; set; }
+        int Downloads { get; set; }
+        DateTime DatePushed { get; set; }
+        Uri ReleaseNotesURI { get; set; }
+        Uri GalleryDownloadURI { get; set; }
+    }
+}
