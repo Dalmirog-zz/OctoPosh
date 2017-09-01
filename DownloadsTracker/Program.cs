e26742e3bf3a501b51996561d5417523a686cad5
dalmirog <dalmiro.granias@gmail.com>
2017-09-01 03:14:16 -0300

    first working version of DownloadsTracker

--- DownloadsTracker/Program.cs
+++ DownloadsTracker/Program.cs
@@ -1,12 +1,34 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System.Linq;
+using DownloadsTracker.Model;
 
 namespace DownloadsTracker
-{
-    class Program
+{    
+    public class Program
     {
-        static void Main(string[] args)
+        internal const string TableName = "DownloadsCount";
+
+        internal static readonly IConfigurationRoot AppConfig = GetConfig();
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var storageConnectionString = AppConfig.GetSection("storageConnectionString").Value;
+
+            var tableClient = new TableClient(storageConnectionString,TableName);
+            
+            var galleryClient = new GalleryClient();
+
+            var latestGalleryEntry = galleryClient.GetEntries().FirstOrDefault();
+
+            tableClient.InsertOrMergeGalleryEntry(latestGalleryEntry);
+        }
+
+        private static IConfigurationRoot GetConfig()
+        {
+            return new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true).Build();
         }
     }
+
 }
+
