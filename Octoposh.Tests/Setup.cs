1df4a620f2151827f40366319a1f7c3dd7204edd
Dalmiro Granas <dalmiro.granias@gmail.com>
2017-09-21 17:51:18 -0300

    Tests tweaks to comply with TestDataGenerator console

---
+++ Octoposh.Tests/Setup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Octopus.Client;
+
+namespace Octoposh.Tests
+{
+    [SetUpFixture]
+    public class Setup
+    {
+        [OneTimeSetUp]
+        public void SetApiKey()
+        {
+            SetApiKeyEnvVariable();
+        }
+
+        private static void SetApiKeyEnvVariable()
+        {
+            var octopusUrl = string.Concat("http://localhost:", ConfigurationManager.AppSettings["OctopusBindingPort"]);
+            var octopusUser = ConfigurationManager.AppSettings["OctopusUser"];
+            var octopusPassword = ConfigurationManager.AppSettings["OctopusPassword"];
+
+            var repository = new OctopusRepository(new OctopusServerEndpoint(octopusUrl));
+
+            repository.Users.SignIn(octopusUser, octopusPassword, true);
+
+            var user = repository.Users.GetCurrent();
+
+            var apiKeyPurpose = "API Key for Unit tests";
+
+            var existingAPIkey = repository.Users.GetApiKeys(user).FirstOrDefault(ak => ak.Purpose == apiKeyPurpose);
+
+            if (existingAPIkey != null)
+            {
+                repository.Users.RevokeApiKey(existingAPIkey);
+            }
+
+            var octopusApiKey = repository.Users.CreateApiKey(user, apiKeyPurpose).ApiKey;
+
+            Environment.SetEnvironmentVariable("OctopusURL", octopusUrl);
+            Environment.SetEnvironmentVariable("OctopusAPIKey", octopusApiKey);
+        }
+    }
+}
