e00da96ade988c1303d282642a53e722b02d1a2a
Dalmiro Granas <dalmiro.granias@gmail.com>
2017-09-21 17:56:43 -0300

    Getting rid of OctopusExport

--- OctopusExport/Projects/DashboardTests_Project2/DashboardTests_Project2 - Run script 1 - Run script 1 - Script.ps1
+++
@@ -1,3 +0,0 @@
-$StepName = $OctopusParameters['Octopus.Step.Name']
-$TargetName = $OctopusParameters['Octopus.Machine.Name']
-"Hello from step [$StepName] on [$Targetname]"
--- OctopusExport/Projects/ProjectTests_Project1/ProjectTests_Project1 - Step to fail on stage - Step to fail on stage - Script.ps1
+++
@@ -1,4 +0,0 @@
-if("Stage" -eq $OctopusParameters['Octopus.Environment.Name']){
-    throw "Failing step to create test data"    
-
-}
